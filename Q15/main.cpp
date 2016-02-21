6b25924952590f5e2dc9f62a7309528b77dbbc5d
Kingpin007 <anirudhkanabar@gmail.com>
2016-02-21 21:03:18 +0530

    Added Q15 - not yet complete

---
+++ Q15/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class LinkedList
+{
+private:
+    struct Node
+    {
+        int data;
+        Node *next;
+    };
+    Node *start, *end;
+public:
+    LinkedList()
+    {
+        start = NULL;
+        end = NULL;
+    }
+    void insert(int x)    //Inserts at the beginning
+    {
+        Node *t = new Node();
+        t->data = x;
+        t->next = NULL;
+        if(start == NULL)
+        {
+            start = t;
+            end = t;
+            t->next = t;
+        }
+        else
+        {
+            t->next = start;
+            start = t;
+            end->next = start;
+        }
+    }
+    void del()       //deletes from the start
+    {
+        Node *t = start;
+        while(t != end)
+            t = t->next;
+        //We have now iterated till the last node the next one is the same as start
+        t->next = start->next;
+        Node *d = start;
+        start = start->next;
+        delete d;
+    }
+    void display()
+    {
+        Node *t = start;
+        if(start != NULL)
+        {
+            while(t->next != start)
+            {
+                cout<<t->data<<"->";
+                t = t->next;
+                system("pause");
+            }
+        }
+        t = t->next;
+        cout<<t->data<<"wrap around\n";
+    }
+    int count()
+    {
+        Node *t = start;
+        int c = 0;
+        if(start != NULL)
+        {
+            while(t->next != start)
+            {
+                c++;
+                t = t->next;
+            }
+        }
+        return c;
+    }
+};
+
+int main()
+{
+    LinkedList l;
+    l.insert(1);
+    l.insert(2);
+    l.insert(3);
+    l.insert(4);
+    l.display();
+    cout<<l.count()<<endl;
+    l.del();
+    cout<<l.count()<<endl;
+    return 0;
+}
